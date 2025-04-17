a3d8d50b98646e1706c2b0ccf0b0ab0c22fc6ce4
Nitin Dube <nitindube227@gmail.com>
2025-04-17 14:13:33 +0530

    fix data

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -15,14 +15,14 @@ export const metadata: Metadata = {
 
 export default function Ecommerce() {
   return (
-    <div className="grid grid-cols-12 gap-4 md:gap-6">
+    <div className="grid grid-cols-6 gap-4 md:gap-6">
       <div className="col-span-12 space-y-6 xl:col-span-7">
         <EcommerceMetrics />
 
-        <MonthlySalesChart />
+        {/* <MonthlySalesChart /> */}
       </div>
 
-      <div className="col-span-12 xl:col-span-5">
+      {/* <div className="col-span-12 xl:col-span-5">
         <MonthlyTarget />
       </div>
 
@@ -36,7 +36,7 @@ export default function Ecommerce() {
 
       <div className="col-span-12 xl:col-span-7">
         <RecentOrders />
-      </div>
+      </div> */}
     </div>
   );
 }
