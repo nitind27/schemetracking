7db8c4b88fbfcd5e0476f3dbbeac00abdb0c934d
Nitin dube <nitindube227@gmail.com>
2025-08-19 11:50:33 +0530

    add

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,9 +22,9 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    '/',
-    '/((?!api|_next/static|_next/image|.*\\.png$|.*\\.svg$).*)',
-    '/uploads/presentwork/:path*'
-  ]
+matcher: [
+  '/',
+  '/((?!api|_next/static|_next/image|uploads/presentwork|.*\\.png$|.*\\.svg$).*)'
+]
+
 };
