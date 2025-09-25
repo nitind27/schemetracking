42b12b08d6df45aee401b864a8ec0acf02d5d35e
Nitin Dube <nitindube227@gmail.com>
2025-09-25 16:36:30 +0530

    add

--- src/app/api/farmernewapi/route.ts
+++ src/app/api/farmernewapi/route.ts
@@ -43,7 +43,7 @@ export async function POST(request: Request) {
     // ];
     const updatableFields = [
         'village_id', 'taluka_id', 'documents',
-        'schemes', 'update_record', 'gis', 'geo_photo',
+        'schemes', 'questions', 'update_record', 'gis', 'geo_photo',
         'farmer_record'
     ];
 
